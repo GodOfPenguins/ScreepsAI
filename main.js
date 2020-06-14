54cea87c287b81776c3c62874a1e4433a0599854
Jeffrey Clark <jmclark85@gmail.com>
2020-06-14 00:58:15 -0400

    Refined role allocation
    Went to a more compact allocation of roles algorithm.  I was trying to preserve which BUBs were in a role, but I'm not fluent enough with Javascript to navigate the Hashmaps yet and make that work without breaking. Functionally, it probably doesn't actually matter.

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const roleBuilder = require('role.builder');
 var harvesters;
 var upgraders;
 var builders;
+var bubs;
+var roomEnergyAvailablePercent;
+var numBUBCreeps;
+var numConSites;
+
 
 module.exports.loop = function () {
 
@@ -12,10 +17,11 @@ module.exports.loop = function () {
     
     // Variables for general state information
     getBuBRoles();
-    var numConSites = Object.keys(Game.constructionSites).length
-    var numBUBCreeps = _.filter(Game.creeps, (creep) => creep.memory.buildType == 'BUB').length;
-    var roomEnergyAvailablePercent = Game.spawns['Spawn1'].room.energyAvailable / Game.spawns['Spawn1'].room.energyCapacityAvailable;
+    numConSites = Object.keys(Game.constructionSites).length
+    numBUBCreeps = _.filter(Game.creeps, (creep) => creep.memory.buildType == 'BUB').length;
+    roomEnergyAvailablePercent = Game.spawns['Spawn1'].room.energyAvailable / Game.spawns['Spawn1'].room.energyCapacityAvailable;
     var roomEnergyAvailable = Game.spawns['Spawn1'].room.energyAvailable;
+    bubs = _.filter(Game.creeps, (creep) => creep.memory.buildType == 'BUB');
     
     // Console output for debugging and status monitoring
     console.log('Harvesters: ' + harvesters.length);
@@ -24,6 +30,7 @@ module.exports.loop = function () {
     console.log('Construction Sites: ' + numConSites)
     console.log('BUBs: ' + numBUBCreeps);
     console.log('Energy Available: ' + roomEnergyAvailable + ', ' + roomEnergyAvailablePercent);
+    console.log(Game.time)
 
     // Basic build logic, this creates one creep for every 50 energy available in the room. I have no basis to know if that's a good pace for expansion or not.
     if(numBUBCreeps < (roomEnergyAvailable / 50)) {
@@ -33,56 +40,11 @@ module.exports.loop = function () {
             {memory: {role: 'harvester', buildType: 'BUB'}});
     }
 
-    {
-        // At some point -- soon -- I need to make more elegant calculations based on how much energy a BUB can actually harvest, 
-        //and how much work/energy is needed for building and filling capacity. That'll be more expandable as I grow and introduce more build types into the fray. 
-        let bubHarvNeed = Math.ceil((1 - roomEnergyAvailablePercent) * numBUBCreeps); // Harvesters are allocated based on how much (by percent) the room is empty on energy.
-        let bubBuildNeed = Math.ceil(numConSites / 4); // Builders are allocated based on the number of empty build sites.
-        let harvAlloc = [];
-        let buildAlloc = [];
-        let diffHarv = harvesters.length - bubHarvNeed;
-        let diffBuild =  builders.length - bubBuildNeed;
-        if(diffHarv > 0){
-            while (diffHarv > 0){ // If more harvesters than needed
-                i = 0;
-                for(h in harvesters){
-                    while(i < bubHarvNeed){
-                        i++;
-                    }
-                    if (diffBuild < 0){
-                        h.role.memory = 'builder';
-                        diffBuild++
-                    }
-                    else{
-                        h.role.memory = 'updater';
-                    }  
-                }
-            }
-        getBuBRoles();
-        }
-        else if(diffHarv < 0){
-            while (diffHarv < 0){ // If fewer harvesters than needed
-                let need = Math.abs(diffHarv);
-                if(diffBuild > 0){ //Taking from surplus builders
-                    let i = 0;
-                    while(i < (diffBuild || need)){
-                        builders[i].role.memory = 'harvester';
-                    }
-                    diffHarv += i;
-                    diffBuild -= i
-                }
-                if (diffBuild <= 0){
-                    for (u in upgraders){
-                        u.memory.role = 'harvester'
-                        diffHarv++
-                    }
-                }
-            }
-        getBuBRoles();
-        }
+    if ((Game.time % 0) === 0){
+        allocateRoles();
+    }
+    
 
-
-        
     // Run each creep according to its role.
     for (let name in Game.creeps){
         let creep = Game.creeps[name];
@@ -102,10 +64,30 @@ module.exports.loop = function () {
         }
     }
 }   
-}
 
 function getBuBRoles(){ // This is a helper function to get how many BUBs are working in each role.
     harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+}
+
+function allocateRoles(){
+    // At some point -- soon -- I need to make more elegant calculations based on how much energy a BUB can actually harvest, 
+    //and how much work/energy is needed for building and filling capacity. That'll be more expandable as I grow and introduce more build types into the fray. 
+    let bubHarvNeed = Math.ceil((1 - roomEnergyAvailablePercent) * numBUBCreeps); // Harvesters are allocated based on how much (by percent) the room is empty on energy.
+    let bubBuildNeed = Math.ceil(numConSites / 4); // Builders are allocated based on the number of empty build sites.
+
+    for (let b in bubs){
+        if (bubHarvNeed > 0){
+            bubs[b].memory.role = 'harvester';
+            bubHarvNeed--;
+        }
+        else if (bubBuildNeed > 0){
+            bubs[b].memory.role = 'builder';
+            bubBuildNeed--;
+        }
+        else{
+            bubs[b].memory.role = 'upgrader'
+        }
+    }
 }
